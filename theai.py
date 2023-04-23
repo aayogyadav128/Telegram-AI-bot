aa25ed39f0d457ad275a49b0547ba91dc316e6a9
Aayog <59125606+aayogyadav128@users.noreply.github.com>
2023-04-23 06:46:17 +0530

    Create theai.py

---
+++ theai.py
@@ -0,0 +1,39 @@
+import openai
+from db import *
+
+openai.api_key = "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"
+
+models={"Shriya (AI Assistant)":"An Ai Assistant Named Shriya",
+       "Code Assitant":"A code Assitant",
+       "Startup Idea Generator":"Startup Idea Generator bot",
+       "Relationship coach":"A Relationship Coach named Jeetu",
+       "CV Builder":"CV Builder Robot",
+       "Teacher":"Teacher named alakh",
+       "A.P.J Abdul Kalam":"Indian Scientist, Dr. A.P.J Abdul Kalam",
+       "Albert Einstien":"Albert Einstien",
+       "Sandeep Maheshwari":"Motivational Speaker named Sandeep Maheshwari",
+       "Narendra Modi":"Prime Minister of India, Narendra Modi"}
+
+def ask_shriya(question,id,model):
+  actor=models.get(model)  
+  print(actor)
+  dialog_messages=fetch_last_three_conversation(id)
+  message=[]
+  message.append({"role": "system", "content": f"you are {actor}"})
+  for dialog_message in dialog_messages:
+    if 'user' in dialog_message:
+      message.append({"role": "user", "content": dialog_message["user"]})
+    else:
+      message.append({"role": "assistant", "content": dialog_message["bot"]})
+  message.append({"role": "user", "content": question})
+  print(message)
+  response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=message,
+    temperature=0.7,
+    frequency_penalty=0,
+    max_tokens=1000,
+    presence_penalty=0
+  )
+  return response
+  
